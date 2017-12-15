8672aa7032e85f3d5fc0742ba1febd38e3d55ec1
zuvizudar <zuvizudar@yahoo.co.jp>
2017-12-15 13:31:15 +0900

    add analyze_recv in ping

--- ping/ping.c
+++ ping/ping.c
@@ -10,24 +10,9 @@
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
 
-unsigned short checksum(unsigned short *buf, int bufsz){
-  unsigned long sum = 0;
+unsigned short checksum(unsigned short *buf, int bufsz);
+void analyze_recv(struct iphdr *iphdrptr,struct icmphdr *icmphdrptr);
 
-  while (bufsz > 1) {
-    sum += *buf;
-    buf++;
-    bufsz -= 2;
-  }
-
-  if (bufsz == 1) {
-    sum += *(unsigned char *)buf;
-  }
-
-  sum = (sum & 0xffff) + (sum >> 16);
-  sum = (sum & 0xffff) + (sum >> 16);
-
-  return ~sum;
-}
 
 int main(int argc,char *argv[]){
 	int soc;
@@ -70,6 +55,7 @@ int main(int argc,char *argv[]){
 	}
 	iphdrptr=(struct iphdr *)buf;
 	icmphdrptr=(struct icmphdr *)(buf + iphdrptr->ihl *4 );
+	analyze_recv(iphdrptr,icmphdrptr);
 	if(icmphdrptr->type==ICMP_ECHOREPLY){
 		printf("OK");
 	}
@@ -78,6 +64,61 @@ int main(int argc,char *argv[]){
 	}
 	close(soc);
 	return 0;
+}
+
+void analyze_recv(struct iphdr *iphdrptr,struct icmphdr *icmphdrptr){
+	struct in_addr *saddr=NULL;
+	struct in_addr *daddr=NULL;
+	char ip_str[18]={};
+
+	printf("ihl   %d\n", iphdrptr->ihl);
+	printf("ver   %d\n", iphdrptr->version);
+	printf("tos   %d\n", iphdrptr->tos);
+	printf("len   %d\n", ntohs(iphdrptr->tot_len));
+	printf("id    %d\n", ntohs(iphdrptr->id));
+	printf("frag  %d\n", iphdrptr->frag_off);
+	printf("ttl   %d\n", iphdrptr->ttl);
+	printf("proto %d\n", iphdrptr->protocol);
+	printf("check %d\n", iphdrptr->check);
+
+	//printf("saddr %d\n", iphdrptr->saddr);
+	//printf("daddr %d\n", iphdrptr->daddr);
+	printf("src ip:");
+	saddr=(struct in_addr*)&(iphdrptr->saddr);
+	inet_ntop(AF_INET,saddr,&ip_str[0],(socklen_t)sizeof(ip_str));
+	printf("%s\n",ip_str);
+	memset(&ip_str[0],0x00,sizeof(ip_str));
+	printf("dst ip:");
+	daddr=(struct in_addr*)&(iphdrptr->daddr);
+	inet_ntop(AF_INET,daddr,&ip_str[0],(socklen_t)sizeof(ip_str));
+	printf("%s\n",ip_str);
+
+
+	printf("type  %d\n", icmphdrptr->type);
+	printf("code  %d\n", icmphdrptr->code);
+	printf("check %d\n", icmphdrptr->checksum);
+	printf("id    %d\n", icmphdrptr->un.echo.id);
+	printf("seq   %d\n", icmphdrptr->un.echo.sequence);
+
+}
+ 
+unsigned short checksum(unsigned short *buf, int bufsz){
+  unsigned long sum = 0;
+
+  while (bufsz > 1) {
+    sum += *buf;
+    buf++;
+    bufsz -= 2;
+  }
+
+  if (bufsz == 1) {
+    sum += *(unsigned char *)buf;
+  }
+
+  sum = (sum & 0xffff) + (sum >> 16);
+  sum = (sum & 0xffff) + (sum >> 16);
+
+  return ~sum;
 }
 
 
